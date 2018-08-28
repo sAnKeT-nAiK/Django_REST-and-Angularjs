3ce4580f8da5dc0292b48fd18f15dca92f60861a
sAnKeT-nAiK <1100sanket@gmail.com>
2018-08-28 23:28:50 +0530

    frf

--- djangular/urls.py
+++ djangular/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'scrumboard/', include('scrumboard.urls')),
+    url('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^scrumboard/', include('scrumboard.urls')),
 ]
